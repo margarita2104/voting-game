e2778816d63cd9d55947178750b9d6a981be002a
margarita2104 <margarita.p.ferreira@gmail.com>
2025-04-29 20:46:02 +0200

    missed an import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import VotingPage from "./components/VotingPage";
 import ResultsPage from "./components/ResultsPage";
+import PlayerSetupPage from "./components/PlayerSetupPage";
 
 function App() {
   return (
